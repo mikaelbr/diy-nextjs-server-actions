4bb67ab7a9d2c1a37f8fc727ce79b540e609e5b2
Mikael Brevik <mikaelbre@gmail.com>
2023-10-21 13:21:35 +0200

    fix: typings for tools

--- form-example-diy/action-utils.js
+++ form-example-diy/action-utils.js
@@ -1,7 +1,17 @@
+/**
+ *
+ * @param {{ file: string; method: string }} input
+ * @returns {string}
+ */
 module.exports.toId = function toId(input) {
   return `${input.file.replace("/", "_")}$${input.method}`;
 };
 
+/**
+ *
+ * @param {string} id
+ * @returns {{ file: string; method: string }}
+ */
 module.exports.fromId = function fromId(id) {
   const splitted = id.split("$");
 
