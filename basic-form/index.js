432353f8b0a265da3f8048e345c570872804c55e
Mikael Brevik <mikaelbre@gmail.com>
2023-10-21 14:13:05 +0200

    feat: adds basic form example

---
+++ basic-form/index.js
@@ -0,0 +1,18 @@
+import { createServer } from "node:http";
+import { createReadStream } from "node:fs";
+import formidable from "formidable";
+
+const server = createServer(async function (req, res) {
+  console.log(new Date(), req.method, req.url);
+
+  if (req.method == "POST") {
+    const form = formidable();
+    const [fields] = await form.parse(req);
+    console.log(new Date(), fields);
+  }
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+  createReadStream("./index.html").pipe(res);
+});
+
+server.listen(3003, () => console.log("Listening on http://localhost:3003"));
